bc8862617767f507be88ed2d4700e2fe05828cbf
amairani <124763500+amairanihernandez@users.noreply.github.com>
2023-07-29 13:52:14 -0600

    lambda

---
+++ lambda/index.js
@@ -0,0 +1,298 @@
+/* *
+ * This sample demonstrates handling intents from an Alexa skill using the Alexa Skills Kit SDK (v2).
+ * Please visit https://alexa.design/cookbook for additional examples on implementing slots, dialog management,
+ * session persistence, api calls, and more.
+ * */
+const Alexa = require('ask-sdk-core');
+
+
+const DOCUMENT_ID = "bienvenida_new";
+const DOCUMENT_ID_1 = "accion1";
+const DOCUMENT_ID_2 = "comedia";
+const DOCUMENT_ID_3 = "drama";
+const DOCUMENT_ID_4 = "no_encontrada";
+const DOCUMENT_ID_5 = "hola";
+const DOCUMENT_ID_6 = "ayuda";
+const DOCUMENT_ID_7 = "adios";
+
+
+
+
+const createDirectivePayload = (aplDocumentId, dataSources = {}, tokenId = "documentToken") => {
+    return {
+        type: "Alexa.Presentation.APL.RenderDocument",
+        token: tokenId,
+        document: {
+            type: "Link",
+            src: "doc://alexa/apl/documents/" + aplDocumentId
+        }
+    }
+};
+
+
+
+
+const LaunchRequestHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'LaunchRequest';
+    },
+    handle(handlerInput) {
+        const speakOutput = 'Hola, te puedo recomendar algunas peliculas, ingresa la categoria que gustes';
+
+        if (Alexa.getSupportedInterfaces(handlerInput.requestEnvelope)['Alexa.Presentation.APL']) {
+            // generate the APL RenderDocument directive that will be returned from your skill
+            const aplDirective = createDirectivePayload(DOCUMENT_ID);
+            // add the RenderDocument directive to the responseBuilder
+            handlerInput.responseBuilder.addDirective(aplDirective);
+        }
+
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt(speakOutput)
+            .getResponse();
+    }
+};
+
+
+const CategoryIntentHandler = {
+  canHandle(handlerInput) {
+    return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+      && Alexa.getIntentName(handlerInput.requestEnvelope) === 'CategoryIntent';
+  },
+  handle(handlerInput) {
+    const category = Alexa.getSlotValue(handlerInput.requestEnvelope, 'category');
+
+    let speakOutput;
+
+    if (category === 'accion') {
+        
+      speakOutput = 'Te recomiendo las siguentes películas de acción como "Avatar", "Ragnarok", entre otras';
+      
+      if (Alexa.getSupportedInterfaces(handlerInput.requestEnvelope)['Alexa.Presentation.APL']) {
+            // generate the APL RenderDocument directive that will be returned from your skill
+            const aplDirective = createDirectivePayload(DOCUMENT_ID_1);
+            // add the RenderDocument directive to the responseBuilder
+            handlerInput.responseBuilder.addDirective(aplDirective);
+        }
+      
+      return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt(speakOutput)
+            .getResponse();
+            
+    } else if (category === 'comedia') {
+        
+        if (Alexa.getSupportedInterfaces(handlerInput.requestEnvelope)['Alexa.Presentation.APL']) {
+            // generate the APL RenderDocument directive that will be returned from your skill
+            const aplDirective = createDirectivePayload(DOCUMENT_ID_2);
+            // add the RenderDocument directive to the responseBuilder
+            handlerInput.responseBuilder.addDirective(aplDirective);
+        }
+        
+      speakOutput = '¡Genial! A todos nos encanta reír. Puedes ver películas como "Mi Pobre Angelito" o "Supercool".';
+      
+      return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt(speakOutput)
+            .getResponse();
+    } else if (category === 'drama') {
+        
+        if (Alexa.getSupportedInterfaces(handlerInput.requestEnvelope)['Alexa.Presentation.APL']) {
+            // generate the APL RenderDocument directive that will be returned from your skill
+            const aplDirective = createDirectivePayload(DOCUMENT_ID_3);
+            // add the RenderDocument directive to the responseBuilder
+            handlerInput.responseBuilder.addDirective(aplDirective);
+        }
+        
+        
+      speakOutput = 'Si te gusta el drama, te sugiero películas como "El Padrino" o "Forrest Gump".';
+      
+      return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt(speakOutput)
+            .getResponse();
+    } else {
+        
+        if (Alexa.getSupportedInterfaces(handlerInput.requestEnvelope)['Alexa.Presentation.APL']) {
+            // generate the APL RenderDocument directive that will be returned from your skill
+            const aplDirective = createDirectivePayload(DOCUMENT_ID_4);
+            // add the RenderDocument directive to the responseBuilder
+            handlerInput.responseBuilder.addDirective(aplDirective);
+        }
+        
+      speakOutput = 'Lo siento, no tengo recomendaciones para esa categoría de películas.';
+      
+      return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt(speakOutput)
+            .getResponse();
+    }
+
+    return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt(speakOutput)
+            .getResponse();
+  }
+};
+
+
+
+
+const HelloWorldIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'HelloWorldIntent';
+    },
+    handle(handlerInput) {
+        const speakOutput = 'Hola, ¿Como te encuentras el dia de hoy?';
+        
+        if (Alexa.getSupportedInterfaces(handlerInput.requestEnvelope)['Alexa.Presentation.APL']) {
+            // generate the APL RenderDocument directive that will be returned from your skill
+            const aplDirective = createDirectivePayload(DOCUMENT_ID_5);
+            // add the RenderDocument directive to the responseBuilder
+            handlerInput.responseBuilder.addDirective(aplDirective);
+        }
+        
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
+            .getResponse();
+    }
+};
+
+const HelpIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.HelpIntent';
+    },
+    handle(handlerInput) {
+        const speakOutput = 'En que te puedo ayudar?';
+
+        if (Alexa.getSupportedInterfaces(handlerInput.requestEnvelope)['Alexa.Presentation.APL']) {
+            // generate the APL RenderDocument directive that will be returned from your skill
+            const aplDirective = createDirectivePayload(DOCUMENT_ID_6);
+            // add the RenderDocument directive to the responseBuilder
+            handlerInput.responseBuilder.addDirective(aplDirective);
+        }
+        
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt(speakOutput)
+            .getResponse();
+    }
+};
+
+const CancelAndStopIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && (Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.CancelIntent'
+                || Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.StopIntent');
+    },
+    handle(handlerInput) {
+        const speakOutput = 'Adios!';
+        
+        if (Alexa.getSupportedInterfaces(handlerInput.requestEnvelope)['Alexa.Presentation.APL']) {
+            // generate the APL RenderDocument directive that will be returned from your skill
+            const aplDirective = createDirectivePayload(DOCUMENT_ID_7);
+            // add the RenderDocument directive to the responseBuilder
+            handlerInput.responseBuilder.addDirective(aplDirective);
+        }
+        
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .getResponse();
+    }
+};
+/* *
+ * FallbackIntent triggers when a customer says something that doesn’t map to any intents in your skill
+ * It must also be defined in the language model (if the locale supports it)
+ * This handler can be safely added but will be ingnored in locales that do not support it yet 
+ * */
+const FallbackIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.FallbackIntent';
+    },
+    handle(handlerInput) {
+        const speakOutput = 'Disculpa, eso no lo se.';
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt(speakOutput)
+            .getResponse();
+    }
+};
+/* *
+ * SessionEndedRequest notifies that a session was ended. This handler will be triggered when a currently open 
+ * session is closed for one of the following reasons: 1) The user says "exit" or "quit". 2) The user does not 
+ * respond or says something that does not match an intent defined in your voice model. 3) An error occurs 
+ * */
+const SessionEndedRequestHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'SessionEndedRequest';
+    },
+    handle(handlerInput) {
+        console.log(`~~~~ Session ended: ${JSON.stringify(handlerInput.requestEnvelope)}`);
+        // Any cleanup logic goes here.
+        return handlerInput.responseBuilder.getResponse(); // notice we send an empty response
+    }
+};
+/* *
+ * The intent reflector is used for interaction model testing and debugging.
+ * It will simply repeat the intent the user said. You can create custom handlers for your intents 
+ * by defining them above, then also adding them to the request handler chain below 
+ * */
+const IntentReflectorHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest';
+    },
+    handle(handlerInput) {
+        const intentName = Alexa.getIntentName(handlerInput.requestEnvelope);
+        const speakOutput = `You just triggered ${intentName}`;
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
+            .getResponse();
+    }
+};
+/**
+ * Generic error handling to capture any syntax or routing errors. If you receive an error
+ * stating the request handler chain is not found, you have not implemented a handler for
+ * the intent being invoked or included it in the skill builder below 
+ * */
+const ErrorHandler = {
+    canHandle() {
+        return true;
+    },
+    handle(handlerInput, error) {
+        const speakOutput = 'Sorry, I had trouble doing what you asked. Please try again.';
+        console.log(`~~~~ Error handled: ${JSON.stringify(error)}`);
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt(speakOutput)
+            .getResponse();
+    }
+};
+
+/**
+ * This handler acts as the entry point for your skill, routing all request and response
+ * payloads to the handlers above. Make sure any new handlers or interceptors you've
+ * defined are included below. The order matters - they're processed top to bottom 
+ * */
+exports.handler = Alexa.SkillBuilders.custom()
+    .addRequestHandlers(
+        LaunchRequestHandler,
+        CategoryIntentHandler,
+        HelloWorldIntentHandler,
+        HelpIntentHandler,
+        CancelAndStopIntentHandler,
+        FallbackIntentHandler,
+        SessionEndedRequestHandler,
+        IntentReflectorHandler)
+    .addErrorHandlers(
+        ErrorHandler)
+    .withCustomUserAgent('sample/hello-world/v1.2')
+    .lambda();
